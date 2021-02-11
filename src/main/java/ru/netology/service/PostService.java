06a3010bf708271eff42e3a274bbffd921e22555
Hronoz87 <hromov_andrey@mail.ru>
2021-02-11 16:34:48 +0300

    Init

---
+++ src/main/java/ru/netology/service/PostService.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.springframework.stereotype.Service;
+import ru.netology.model.Post;
+import ru.netology.repository.PostRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+  private final PostRepository repository;
+
+  public PostService(PostRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Post> all() {
+    return repository.all();
+  }
+
+  public Optional<Post> getById(long id) {
+    return repository.getById(id);
+  }
+
+  public Post save(Post post) {
+    return repository.save(post);
+  }
+
+  public void removeById(long id) {
+    repository.removeById(id);
+  }
+}
+
